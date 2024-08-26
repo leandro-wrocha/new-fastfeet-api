4477d29b98616ba9eeca5053ad06a53cf0fba248
Leandro Rocha <leandro.wrocha98@gmail.com>
2024-08-26 20:54:43 -0300

    feat: adicao das classes aggregate-root e unique-entity-id, para agregar minha entidade.

--- src/domain/people/enterprise/entities/admin.ts
+++ src/domain/people/enterprise/entities/admin.ts
@@ -1,49 +1,48 @@
-import { randomUUID } from 'node:crypto';
+import { AggregateRoot } from 'src/core/entities/aggregate-root';
+import { UniqueEntityId } from 'src/core/entities/unique-entity-id';
 
 interface AdminProps {
-  id?: string;
+  id?: UniqueEntityId;
   name: string;
   cpf: string;
   password: string;
   createdAt?: Date;
 }
 
-export class Admin {
-  constructor(private adminProps: AdminProps) {}
-
-  get id() {
-    return this.adminProps.id;
+export class Admin extends AggregateRoot<AdminProps> {
+  constructor(props: AdminProps, id?: UniqueEntityId) {
+    super(props, id);
   }
 
   get name() {
-    return this.adminProps.name;
+    return this.props.name;
   }
 
   get cpf() {
-    return this.adminProps.cpf;
+    return this.props.cpf;
   }
 
   get password() {
-    return this.adminProps.password;
+    return this.props.password;
   }
 
   get createdAt() {
-    return this.adminProps.createdAt;
+    return this.props.createdAt;
   }
 
   set name(value: string) {
-    this.adminProps.name = value;
+    this.props.name = value;
   }
 
   set cpf(value: string) {
-    this.adminProps.cpf = value;
+    this.props.cpf = value;
   }
 
   set password(value: string) {
-    this.adminProps.password = value;
+    this.props.password = value;
   }
 
   static create(props: AdminProps) {
-    return new Admin({ ...props, id: randomUUID(), createdAt: new Date() });
+    return new Admin({ ...props, createdAt: new Date() }, new UniqueEntityId());
   }
 }
